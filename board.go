2167ec061f84642bbb82dfe7d7c47cd1effc1a36
Maks Verver <maksverver@geocities.com>
2013-09-14 00:42:45 +0200

    Update board file format. Support swap moves. Add documentation.

--- board.go
+++ board.go
@@ -6,30 +6,43 @@ import (
 	"io"
 )
 
+// Maximum number of fields on a Poly-Y board.
 const MAX_FIELDS = 1000000
+
+// Maximum number of sides for a Poly-Y board.
+// (Chosen to be less than the width of an integer, to facilitate bitmasking.)
 const MAX_SIDES = 31
 
+// A Poly-Y board is an undirected graph with fields (indexed from 0) and sides
+// (which are subsets of fields).  In the Board struct, Fields is an array
+// of adjacency lists; i.e. Fields[i][j] == k implies that fields i and j
+// are adjacent.  Sides is an array of arrays of field indices; i.e.
+// Sides[i][j] == k implies that field k lies on the i-th side.  Note that
+// fields may lay on multiple sides!
 type Board struct {
 	Fields [][]int
 	Sides  [][]int
 }
 
+// Reads a list of field indices, converted from 1-based to 0-based.
+// The first integer read indicates the number of indices that follow.
+// e.g. if the input contains "3 2 4 6" readIndices() returns [1 3 5].
 func readIndices(r io.Reader) []int {
-	is := make([]int, 0)
-	for {
-		var i int
-		n, _ := fmt.Fscanf(r, "%d", &i)
-		if n != 1 {
+	var i int
+	if n, _ := fmt.Fscanf(r, "%d", &i); n != 1 {
+		return nil
+	}
+	res := make([]int, i)
+	for i = range res {
+		if n, _ := fmt.Fscanf(r, "%d", &res[i]); n != 1 {
 			return nil
 		}
-		if i == 0 {
-			break
-		}
-		is = append(is, i-1)
+		res[i]-- // decrement values
 	}
-	return is
+	return res
 }
 
+// Reads a Poly-Y board description.
 func (b *Board) Read(r io.Reader) error {
 	var nfield, nside int
 	if n, _ := fmt.Fscanf(r, "Poly-Y %d %d", &nfield, &nside); n != 2 {
@@ -44,11 +57,6 @@ func (b *Board) Read(r io.Reader) error {
 	fields := make([][]int, nfield)
 	sides := make([][]int, nside)
 	for i := range fields {
-		var j int
-		var x, y float64
-		if n, _ := fmt.Fscanf(r, "%d %f %f", &j, &x, &y); n < 3 || j != i+1 {
-			return errors.New("board: invalid field header")
-		}
 		fields[i] = readIndices(r)
 		if fields[i] == nil {
 			return errors.New("board: invalid neighbour indices")
--- game.go
+++ game.go
@@ -1,3 +1,4 @@
+// Poly-Y is the abstract board game Y played on a polygonal board.
 package poly_y
 
 import (
@@ -5,8 +6,12 @@ import (
 	"strconv"
 )
 
+// A move is either a positive (1-based) field index, or the number -1 which
+// requests swapping positions (only allowed as the second move).
 type Move int
 
+// The Poly-Y game state consists of a board descriptor (which is not modified
+// by any method) and an array of valid moves executed so far.
 type State struct {
 	Board *Board
 	Moves []Move
@@ -16,56 +21,84 @@ func (m Move) String() string {
 	return strconv.FormatInt(int64(m), 10)
 }
 
+// Returns whether the game is over.
 func (s *State) Over() bool {
-	if len(s.Moves) >= len(s.Board.Fields) {
-		return true
+	// See if there are any unoccupied fields left:
+	unoccupied := len(s.Board.Fields) - len(s.Moves)
+	if len(s.Moves) >= 2 && s.Moves[1] == -1 {
+		unoccupied++
 	}
+	if unoccupied <= 0 {
+		return false
+	}
+
+	// Check if either player has captured a majority of the corners:
 	a, b := s.Scores()
 	n := len(s.Board.Sides) / 2
 	return a > n || b > n
 }
 
+// Returns which player (0 or 1) must play next.
 func (s *State) Next() int {
 	return len(s.Moves) % 2
 }
 
-func (s *State) Occupied(field int) bool {
+// Returns whether the field with the given 1-based index is occupied.
+func (s *State) occupied(field int) bool {
 	for _, m := range s.Moves {
-		if field == int(m) {
+		i := int(m)
+		if i != -1 && i == int(field) {
 			return true
 		}
 	}
 	return false
 }
 
+// Lists all valid moves in the current state.
 func (s *State) ListMoves() []interface{} {
 	moves := make([]interface{}, 0)
+	if len(s.Moves) == 1 {
+		moves = append(moves, -1)
+	}
 	for i := 1; i <= len(s.Board.Fields); i++ {
-		if !s.Occupied(i) {
+		if !s.occupied(i) {
 			moves = append(moves, Move(i))
 		}
 	}
 	return moves
 }
 
+// Returns whether the given move is valid in the current state.
+func (s *State) valid(m Move) bool {
+	i := int(m)
+	if i == -1 {
+		return len(s.Moves) == 1
+	}
+	return 1 <= i && i <= len(s.Board.Fields) && !s.occupied(i)
+}
+
+// If the argument is a valid move, it is executed, and true is returned.
+// Otherwise, the game state is unchanged, and false is returned.
 func (s *State) Execute(arg interface{}) bool {
-	if m, ok := arg.(Move); ok {
-		i := int(m)
-		if i > 0 && i <= len(s.Board.Fields) && !s.Occupied(i) {
-			s.Moves = append(s.Moves, m)
-			return true
-		}
+	if m, ok := arg.(Move); ok && s.valid(m) {
+		s.Moves = append(s.Moves, m)
+		return true
 	}
 	return false
 }
 
+// Returns the current score (in number of corners captured) for both players.
 func (s *State) Scores() (int, int) {
 	score1, score2 := 0, 0
 	colors := make([]int, len(s.Board.Fields))
 	sides := make([]uint, len(s.Board.Fields))
 	visited := make([]bool, len(s.Board.Fields))
 	for i, m := range s.Moves {
-		colors[int(m)-1] = 1 - i%2*2
+		j := int(m)
+		if j == -1 {
+			j = int(s.Moves[i-1])
+		}
+		colors[j-1] = 1 - i%2*2
 	}
 	for i, side := range s.Board.Sides {
 		for _, j := range side {
@@ -106,6 +139,8 @@ func (s *State) Scores() (int, int) {
 	return score1, score2
 }
 
+// Writes the game transcript.  The format is a sequence of 1-based field
+// indices (with -1 indicating swap) with lines folded to 79 columns.
 func (s *State) WriteLog(w io.Writer) {
 	text := ""
 	col := 0
